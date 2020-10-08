8030cc6c30f1aeb9ce1e05af1d50b770dd3cc5d0
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-10-08 18:24:15 +0545

    environment variables added and changed all jsx

--- src/comps/Title.jsx
+++ src/comps/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>IG</h1>
       <h2>Your Pictures</h2>
       <p>Welcome to the Firegram gallery</p>
     </div>
