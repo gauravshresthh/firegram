761c6390db6b535c9fe851830278e99e06790d0b
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-10-09 01:36:12 +0545

    hover-effects added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import logo from './img/gs-logo.png';
 import Posts from './Pages/Posts';
 import Home from './Pages/Home';
 import { Switch, Route,Link } from 'react-router-dom';
@@ -7,7 +7,11 @@ import { Switch, Route,Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Link to='/' className='app-logo'>IG</Link>
+      <div className='app-logo'> 
+      <Link to='/' ><img src={logo} alt=""/>
+      </Link>
+      </div>
+     
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/posts" component={Posts} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,10 @@ import {Link} from'react-router-dom';
 const Home = () => {
   return (
     <div className="home">
-      <div className="hero-info"><h1>Test the app on click ,<br/> <span className='paper'>not paper</span></h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt nam sequi necessitatibus vero veritatis? Voluptas sed, quo quaerat animi quas, fuga asperiores nulla tempora cum aut magni voluptatibus harum deleniti blanditiis accusamus. Nemo voluptatum quibusdam quis recusandae, unde accusantium placeat beatae eveniet enim quisquam quasi excepturi inventore ratione repellendus facere!</p><Link to='/posts' className='startBtn'>Get Started</Link></div>
+      <div className="hero-info"><h1>Test the app on click ,<br/> <span className='paper'>Realtime Database</span></h1>
+      <p>Instagram type image gallery built using React Js, React Router and Firebase . Used google cloud functions to interact with the realtime database. </p>
+      <Link to='/posts' className='startBtn'>Get Started</Link>
+      </div>
       <div className="hero-img"><img src="https://images.unsplash.com/45/ZLSw0SXxThSrkXRIiCdT_DSC_0345.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80" alt=""/></div>
     </div>
   );
