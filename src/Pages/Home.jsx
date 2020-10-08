8bc4353206d4b7e484e47a154d5786a3f981ec99
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-10-08 18:59:09 +0545

    styling refactored

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,14 +7,14 @@ import Modal from '../Components/Modal';
 const Home = () => {
   const [selectedImg, setSelectedImg] = useState(null);
   return (
-    <>
+    <div>
       <Title />
       <UploadForm />
       <ImageGrid setSelectedImg={setSelectedImg} />
       {selectedImg && (
         <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
       )}
-    </>
+    </div>
   );
 };
 
