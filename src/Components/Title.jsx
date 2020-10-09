9a1c518a492f96685d83793e2399fe7cce497cee
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-10-09 09:46:05 +0545

    refactorized some stylings

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h2>INSTAGRAM</h2>
+      <h2>Firegram</h2>
       <p>
         Click the + Button to add your pictures and see it update in realtime through the
         realtime database
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,11 +3,13 @@ import {Link} from'react-router-dom';
 const Home = () => {
   return (
     <div className="home">
-      <div className="hero-info"><h1>Test the app on click ,<br/> <span className='paper'>Realtime Database</span></h1>
+      <div className="hero-info">
+      <h1>Test the app on click ,<br/> <span className='paper'>Realtime Database</span></h1>
       <p>Instagram type image gallery built using React Js, React Router and Firebase . Used google cloud functions to interact with the realtime database. </p>
       <Link to='/posts' className='startBtn'>Get Started</Link>
       </div>
-      <div className="hero-img"><img src="https://images.unsplash.com/45/ZLSw0SXxThSrkXRIiCdT_DSC_0345.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80" alt=""/></div>
+      <div className="hero-img">
+        <img src="https://images.unsplash.com/45/ZLSw0SXxThSrkXRIiCdT_DSC_0345.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80" alt=""/></div>
     </div>
   );
 };
